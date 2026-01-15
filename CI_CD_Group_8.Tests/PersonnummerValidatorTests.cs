111e288b08bd0fe3de935225dbc7a7b5664e5e46
kerols04 <116015613+kerols04@users.noreply.github.com>
2026-01-15 11:25:40 +0100

    Add unit tests for PersonnummerValidator

--- CI_CD_Group_8.Tests/PersonnummerValidatorTests.cs
+++ CI_CD_Group_8.Tests/PersonnummerValidatorTests.cs
@@ -1,1 +1,27 @@
+using Xunit;
+
+namespace CI_CD_Group_8.Tests
+{
+    public class PersonnummerValidatorTests
+    {
+        [Theory]
+        [InlineData("9001010017")]
+        [InlineData("900101-0017")]
+        public void Valid_Personnummer_ReturnsTrue(string pnr)
+        {
+            Assert.True(CI_CD_Group_8.PersonnummerValidator.IsValid(pnr));
+        }
+
+        [Theory]
+        [InlineData("900101-0018")] // fel kontrollsiffra
+        [InlineData("990231-1234")] // ogiltigt datum
+        [InlineData("abcdef-1234")] // bokstäver
+        [InlineData("")]            // tom
+        [InlineData("123")]         // för kort
+        public void Invalid_Personnummer_ReturnsFalse(string pnr)
+        {
+            Assert.False(CI_CD_Group_8.PersonnummerValidator.IsValid(pnr));
+        }
+    }
+}
 
